2d3d0fe0c598e298404984e8c3c19f3224bc6611
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-06-30 22:37:19 -0500

    feat: agregar dashboard

--- src/containers/app/router/main-route.tsx
+++ src/containers/app/router/main-route.tsx
@@ -4,19 +4,30 @@ import MainLayout from '../../../components/dashboard/main-layout';
 
 const Dashboard = lazy(() => import('../../dashboard/dashboard'));
 const Profile = lazy(() => import('../../profile/profile'));
-const FileUpload = lazy(() => import('../../file-upload/file-upload'));
 const Register = lazy(() => import('../../users/register/register'));
+const Check = lazy(() => import('../../file/check/check'));
+const Sign = lazy(() => import('../../file/sign/sign'));
 
 function MainRoute() {
    const location = useLocation();
    return (
-      <Route path={['/dashboard', '/profile', '/file-upload', '/register']}>
+      <Route
+         path={[
+            '/dashboard',
+            '/profile',
+            '/file-upload',
+            '/register',
+            '/file/check',
+            '/file/sign',
+         ]}
+      >
          <MainLayout>
             <Switch location={location} key={location.pathname}>
                <Route path='/dashboard' component={Dashboard} />
                <Route path='/profile' component={Profile} />
-               <Route path='/file-upload' component={FileUpload} />
                <Route path='/register' component={Register} />
+               <Route path='/file/check' component={Check} />
+               <Route path='/file/sign' component={Sign} />
             </Switch>
          </MainLayout>
       </Route>
---
+++ src/containers/file/sign/sign.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Sign() {
+    return (
+        <div>
+            Desde File sign
+        </div>
+    );
+}
+
+export default Sign;
