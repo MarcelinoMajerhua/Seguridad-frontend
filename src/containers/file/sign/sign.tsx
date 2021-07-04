3400f205983370ba228ccf180c6954dcbe3d613d
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-07-04 16:40:06 -0500

    feat: agregar, componente register

--- src/containers/file/sign/sign.tsx
+++ src/containers/file/sign/sign.tsx
@@ -5,10 +5,14 @@ import CustomBody from '../../../components/body-custom/custom-body';
 import CustomCard from '../../../components/custom-card/custom-card/custom-card';
 import CustomCardHeader from '../../../components/custom-card/custom-card-header/custom-card-header';
 import CustomCardBody from '../../../components/custom-card/custom-card-body/custom-card-body';
-import { Button, Grid } from '@material-ui/core';
+import { Avatar, Button, Grid, Paper, Typography } from '@material-ui/core';
 import CustomTextField from '../../../components/custom-text-field/custom-text-field';
+import DocumentWidgetDropzone from './components/document-widget-dropzone';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import { green } from '@material-ui/core/colors';
 
 function Sign() {
+   const [document, setDocument] = React.useState<any>([]);
    return (
       <React.Fragment>
          <CustomBodyName title={'Componente'} height='150px'>
@@ -35,8 +39,37 @@ function Sign() {
                         <Grid item xs={12} sm={12} md={6}>
                            <CustomTextField label={'Titulo'} name={'title'} />
                         </Grid>
-                        <Grid item xs={12} sm={12} md={12}>
-                           <CustomTextField label={'Por favor suba su archivo'} name={'file'} />
+                        <Grid item xs={12} sm={12} md={6}>
+                           <div style={{ paddingTop: '5px' }}>
+                              <DocumentWidgetDropzone setDocument={setDocument} />
+                           </div>
+                        </Grid>
+                        <Grid item xs={12} sm={12} md={6}>
+                           <Paper style={{ height: '210px' }} elevation={3}>
+                              {document && document.length > 0 && (
+                                 <>
+                                    <div style={{ padding: '10px' }}>
+                                       <Typography>
+                                          <strong>Nombre del Documento:</strong>
+                                       </Typography>
+                                       <Typography>{document[0].name}</Typography>
+                                    </div>
+                                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                       <Avatar
+                                          variant={'rounded'}
+                                          style={{
+                                             color: '#fff',
+                                             backgroundColor: green[500],
+                                             height: '100px',
+                                             width: '100px',
+                                          }}
+                                       >
+                                          <AssignmentIcon style={{ fontSize: 50 }} />
+                                       </Avatar>
+                                    </div>
+                                 </>
+                              )}
+                           </Paper>
                         </Grid>
 
                         <Grid item xs={12} sm={12} md={12}>
