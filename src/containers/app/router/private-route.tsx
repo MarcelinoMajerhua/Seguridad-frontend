6c5a0250bfe43c875f4c3310e45cd897ab8117f9
WiliamRamirez <wiliam.ramirez.27@unsch.edu.pe>
2021-07-06 23:30:28 -0500

    feat: agregar rutas protegidas

---
+++ src/containers/app/router/private-route.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Redirect, Route, RouteComponentProps, RouteProps } from 'react-router-dom';
+import { useStore } from '../../../store/store';
+
+interface Props extends RouteProps {
+   component: React.ComponentType<RouteComponentProps<any> | React.ComponentType<any>>;
+}
+
+function PrivateRoute({ component: Component, ...rest }: Props) {
+   const {
+      userStore: { isLoggedIn },
+   } = useStore();
+   return (
+      <React.Fragment>
+         <Route
+            {...rest}
+            render={(props) => (isLoggedIn ? <Component {...props} /> : <Redirect to={'/'} />)}
+         />
+      </React.Fragment>
+   );
+}
+
+export default PrivateRoute;
